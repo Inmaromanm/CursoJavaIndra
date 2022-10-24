3282fe7969f0d9ed852deb959a40925eb1b603c7
iromanm <iromanm@11LAP5CD2280LLT.indra.es>
2022-10-24 12:52:06 +0200

    Entrega ejercicio AA14

---
+++ EjercicioAA14/src/main/java/com/example/demo/Controlador.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Controlador {
+	@GetMapping("index")
+	public String index() {
+		return "index";
+	}
+	@GetMapping("noticias")
+	public String noticias() {
+		return "noticias";
+	}
+	@GetMapping("tecnologia")
+	public String tecnologia() {
+		return "tecnologia";
+	}
+	@GetMapping("comida")
+	public String comida() {
+		return "comida";
+	}
+	@GetMapping("farandula")
+	public String farandula() {
+		return "farandula";
+	}
+	@GetMapping("deportes")
+	public String deportes() {
+		return "deportes";
+	}
+	@GetMapping("gamming")
+	public String gamming() {
+		return "gamming";
+	}
+	@GetMapping("influencers")
+	public String influencers() {
+		return "influencers";
+	}
+	@GetMapping("musica")
+	public String musica() {
+		return "musica";
+	}
+}
---
+++ EjercicioAA14/src/main/java/com/example/demo/EjercicioAa14Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioAa14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioAa14Application.class, args);
+	}
+
+}
